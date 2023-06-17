936b9b2e0fae317a9dbf06d8933455a9443bd296
Rakeshpudu <117848570+Rakeshpudu@users.noreply.github.com>
2023-06-17 21:26:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-function letterCombinations(input_digit) {
+function letterCombinations(input) {
   //Complete the function
+	let table ={
+		"2" : "abc",
+		"3" : "def",
+		"4" : "ghi",
+		"5" : "jkl",
+		"6" : "mno",
+		"7" : "pqrs",
+		"8" : "tuv",
+		"9" : "wxyz"
+	}
+	let sol=[];
+	function solve(currentIndex,calString){
+		if(input.length <= currentIndex){
+			sol.push(calString);
+			return;
+		}
+		let currentchar = input[currentIndex]
+		for(let i=0;i<table[currentchar].length ; i++)
+			{
+				solve(currentIndex+ 1,calString+ table[currentchar])
+			}
+	}
+	solve(0,"")
+	return sol;
 }
 
 module.exports = letterCombinations;
